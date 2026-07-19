fd7aa087bf2d06f86bb270611fd94528a1a2c641
Vansh1133 <vanshsharma77890@gmail.com>
2026-07-19 17:12:18 +0530

    Time: 0 ms (100%), Space: 45.7 MB (41.74%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -18,11 +18,11 @@ class Solution {
         if(root == null) return true;
         int left = height(root.left);
         int right = height(root.right);
-        if(Math.abs (left-right)>1)return false ;
-        return (isBalanced(root.left) && isBalanced(root.right));
+       if(Math.abs(left-right)>1) return false ;
+        return(isBalanced(root.left) && isBalanced(root.right));
     }
     public int height(TreeNode root ){
-        if(root == null ) return 0;
+        if(root == null) return 0 ;
         return 1+Math.max(height(root.left),height(root.right));
     }
 }
