7c4dc057490db696bea9cab9f13bc08515f9684c
Vansh1133 <vanshsharma77890@gmail.com>
2026-07-23 12:14:09 +0530

    Time: 14 ms (37.58%), Space: 46.9 MB (59.21%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-    int [] scount = new int [26];
-    int [] pcount=  new  int [26];
-    List<Integer> ans = new ArrayList<>();
-    for(char c : p.toCharArray()){
-        pcount[c-'a']++;
-    } 
-    for(int i =0;i<s.length();i++){
-        scount[s.charAt(i) -'a']++;
-    
-    if( i >=p.length()){
-        scount[s.charAt(i-p.length())-'a']--;
-    }
-    if( i>=p.length()-1){
-      if(Arrays.equals(pcount,scount)){
-        ans .add(i-p.length()+1);
-      }
-    }
-    }
-    
-    return ans ;
+        int [] scount = new int [26];
+        int [] pcount = new int [26];
+        List<Integer>ans = new ArrayList<>();
+        for(char c : p.toCharArray()){
+            pcount[c-'a']++;
         }
-                    }
+        for(int i =0;i<s.length();i++){
+          scount[s.charAt(i)-'a']++;
+          if(i>=p.length()){
+            scount[s.charAt(i-p.length())-'a']--;
+          }
+          if(i>=p.length()-1){
+            if(Arrays.equals(pcount,scount)){
+                ans.add(i-p.length()+1);
+            }
+          }
+        }
+       return ans ;
+
+    }
+}
